1cf25ab88fbe63aafa77ce526e023ee970039ea3
MatheusDG6 <matheusdg.2006@gmail.com>
2026-05-19 17:13:29 -0300

    msg

---
+++ src/main/java/com/tela/projetotela/ProjetotelaApplication.java
@@ -0,0 +1,13 @@
+package com.tela.projetotela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetotelaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetotelaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tela/projetotela/ProjetotelaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tela.projetotela;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetotelaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
