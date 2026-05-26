baac5b02a60937ce8e8c9745b7e70295f4a11da6
MatheusDG6 <MatheusDG6>
2026-05-26 17:19:43 -0300

    continuando projeto

---
+++ src/main/java/com/bidding/system/frontend/controller/EditalPageController.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.bidding.system.frontend.controller;
+
+import com.bidding.system.frontend.model.EditalDTO;
+import com.bidding.system.frontend.model.UserRequestDTO;
+import com.bidding.system.frontend.service.AuthRestClientService;
+import jakarta.servlet.http.HttpSession;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+/**
+ *
+ * @author Aluno
+ */
+@Controller
+public class EditalPageController {
+    
+    @Autowired
+    private AuthRestClientService restService;
+    
+    @GetMapping("/")
+    public String home(
+            HttpSession session
+    ) {
+        return "editais";
+    }
+    
+    @GetMapping
+        public List<EditalDTO> listarEditais(
+            @RequestHeader("Authorization") String auth
+        ){
+            String token = auth.replace("Bearer", "");
+            List<EditalDTO> lista = restService.listarEditais(token);
+            return lista;
+    }
+}
